59cd4726ad02784986e18556ed2f17d23f323d71
jpca999 <jpca999@gmail.com>
2013-05-22 15:41:10 -0700

    added adn changed config file

---
+++ config/config.js
@@ -0,0 +1,73 @@
+
+var path = require('path')
+  , rootPath = path.normalize(__dirname + '/..')
+  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
+  , notifier = {
+      APN: false,
+      email: false, // true
+      actions: ['comment'],
+      tplPath: templatePath,
+      postmarkKey: 'POSTMARK_KEY',
+      parseAppId: 'PARSE_APP_ID',
+      parseApiKey: 'PARSE_MASTER_KEY'
+    }
+
+module.exports = {
+  development: {
+    db: 'mongodb://localhost/noobjs_dev',
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "NhNCwt2r41x3AAG9OSH5yQ",
+      clientSecret: "T7rsTCFyKOyby7aeXtt92HK9NizJVBfH3LoV6uVp3A",
+      callbackURL: "http://192.168.16.131:3000/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/google/callback"
+    },
+  },
+  test: {
+    db: 'mongodb://localhost/noobjs_test',
+    root: rootPath,
+    notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/google/callback"
+    }
+  },
+  production: {}
+}
